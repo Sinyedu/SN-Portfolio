c845750964b08eb0e144a59c5bd974c8f45744b2
Sinyedu <simnyb01@easv365.dk>
2024-04-15 15:49:46 +0200

    Somethings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,10 @@
   <div class="flex justify-end">
     <div class="heroimg">
       <img class="h-96 w-96 rounded-full" src="/src/assets/Hero1.jpg" alt="Simon Nyblom">
+      <div class="flex">
+      <img class="h-25 w-32 rounded-3xl mr-3 mt-4" src="/src/assets/htmlicon.png" alt="HTML Icon">
+      <img class="h-25 w-32 rounded-3xl mr-3 mt-4" src="/src/assets/cssicon.png" alt="CSS Icon">
+      </div>
     </div>
   </div>
 
@@ -12,10 +16,18 @@
 </template>
 
 <style scoped>
+
+.flex {
+position: relative;
+right: 25vh;
+}
+
   .heroimg {
+    position: relative;
     width: 200vh;
     display: flex;
-    justify-content: flex-end;
+    align-items: end;
+    flex-direction: column;
   }
 
   .w-96 {
