dfcdb50ede232d10380836435a28daefed96e33f
Sinyedu <simnyb01@easv365.dk>
2024-04-15 12:14:55 +0200

    Picture

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>
-    
+  <div class="flex justify-end">
+    <div class="heroimg">
+      <img class="h-96 w-96 rounded-full" src="/src/assets/Hero1.jpg" alt="Simon Nyblom">
+    </div>
+  </div>
+
+  <div class="herotext">
+    <h1 class="text-4xl font-bold">Simon Erik Nyblom</h1>
+    <p class="text-2xl"> Multimedia Student, with a passion for Frontend Development</p>
   </div>
 </template>
-<style>
+
+<style scoped>
+  .heroimg {
+    width: 200vh;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .w-96 {
+    width: 37rem;
+  }
+
+  .h-96 {
+    height: 37rem;
+  }
 </style>
